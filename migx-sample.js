3ccad93cc8b32873f4b22d8122c9f99c828c9234
Bromik <bromik1987@gmail.com>
2017-05-19 10:51:20 +0300

    Update migx-sample.js

--- migx-sample.js
+++ migx-sample.js
@@ -12,3 +12,17 @@ Table:
 {"header": "Name", "sortable": "true", "dataIndex": "title"},
 {"header": "Photo", "width": "80", "sortable": "false", "dataIndex": "imageTV","renderer": "this.renderImage"}
 ]
+
+//
+[
+{"caption":"Info", "fields": [
+{"field":"image","caption":"Image","inputTVtype":"image"},
+{"field":"title","caption":"Title"}
+]}
+] 
+
+
+[
+{"header": "Title", "width": "160", "sortable": "true", "dataIndex": "title"},
+{"header": "Image", "width": "50", "sortable": "false", "dataIndex": "image","renderer": "this.renderImage"}
+]
