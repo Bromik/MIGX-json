63d839882f2be99098e7f7a45c9e2a2b56f1291e
Bromik <bromik1987@gmail.com>
2017-05-31 11:43:40 +0300

    Update migx-sample.js

--- migx-sample.js
+++ migx-sample.js
@@ -26,3 +26,25 @@ Table:
 {"header": "Title", "width": "160", "sortable": "true", "dataIndex": "title"},
 {"header": "Image", "width": "50", "sortable": "false", "dataIndex": "image","renderer": "this.renderImage"}
 ]
+///
+[
+{"caption":"Info", "fields": [
+{"field":"preficks_name","caption":"R"},
+{"field":"title_lat","caption":"R"},
+{"field":"title_lng","caption":"R"},
+{"field":"image","caption":"Icon","inputTVtype":"iamge"},
+{"field":"title_alt","caption":"R"},
+{"field":"description","caption":"R","inputTVtype":"textarea"}
+]}
+] 
+
+
+
+[
+{"header": "R", "width": "160", "sortable": "true", "dataIndex": "preficks_name"},
+{"header": "R", "width": "160", "sortable": "true", "dataIndex": "title_lat"},
+{"header": "R", "width": "160", "sortable": "true", "dataIndex": "title_lng"},
+{"header": "R", "width": "150", "sortable": "false", "dataIndex": "image","renderer": "this.renderImage"},
+{"header": "R", "width": "160", "sortable": "true", "dataIndex": "title_alt"},
+{"header": "R", "width": "160", "sortable": "true", "dataIndex": "description"}
+]
